eccf223c47eb80f76d19182f7685db829004dc2d
Mohaideen27 <mohaideenmech27@gmail.com>
2026-03-11 19:31:45 +0530

    LEARNED DATATYPES, PRIMITIVE AND OBJECTS IN JAVASCRIPT

---
+++ JAVASCRIPT/VARIABLES, DATATYPES AND OBJECTS/script.js
@@ -0,0 +1,33 @@
+console.log("Javascript Variables, Datatypes and Object Class Started.");
+var a = 10;
+var $a = 20;
+var a_ = 40;
+var a$ = "Variables";
+// console.log(a, $a, a_);
+// console.log(typeof a, typeof $a, typeof a_, typeof a$);
+{
+  let a = 20;
+  // console.log(a);
+}
+// console.log(a);
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+  name: "sameer",
+  course: "full stack python",
+  is_hansome: true
+};
+
+console.log(o);
+o.salary = "50000";
+console.log(o);
+o.salary = "60000";
+console.log(o);
