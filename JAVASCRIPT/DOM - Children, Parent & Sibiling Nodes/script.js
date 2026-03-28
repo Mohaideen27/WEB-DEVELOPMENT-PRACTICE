fc855b7f5bce3e7bb210ee710609135e1901012c
Mohaideen27 <mohaideenmech27@gmail.com>
2026-03-28 23:36:29 +0530

    PRACTICED JAVASCRIPT DOM

--- JAVASCRIPT/DOM - Children, Parent & Sibiling Nodes/script.js
+++ JAVASCRIPT/DOM - Children, Parent & Sibiling Nodes/script.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+document.body.style.background = "wheat";
+
+let span = document.getElementById("box");
+box.style.color = "red";
+
+let con = document.getElementsByClassName("box");
+box.style.background = "aqua";
