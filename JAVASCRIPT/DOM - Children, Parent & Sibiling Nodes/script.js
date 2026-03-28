558034ef97688c48376b689910de510b8bb9afaf
Mohaideen27 <mohaideenmech27@gmail.com>
2026-03-28 23:53:13 +0530

    PRACTICING JAVASCRIPT DOM

--- JAVASCRIPT/DOM - Children, Parent & Sibiling Nodes/script.js
+++ JAVASCRIPT/DOM - Children, Parent & Sibiling Nodes/script.js
@@ -7,3 +7,10 @@ box.style.color = "red";
 
 let con = document.getElementsByClassName("box");
 box.style.background = "aqua";
+
+let head = document.table.head;
+head.style.color = "red";
+let row = document.table.row;
+head.style.color = "grey";
+let data = document.table.data;
+head.style.color = "yellow";
