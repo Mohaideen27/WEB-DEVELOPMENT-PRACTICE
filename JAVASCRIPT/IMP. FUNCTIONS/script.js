4eb0853a9742ef3ec968569c5fcafa57866649f8
Mohaideen27 <mohaideenmech27@gmail.com>
2026-03-29 23:52:07 +0530

    PRACTICING IMP JAVASCRIPT FUNCTIONS

--- JAVASCRIPT/IMP. FUNCTIONS/script.js
+++ JAVASCRIPT/IMP. FUNCTIONS/script.js
@@ -1,6 +1,11 @@
 let box = document.querySelector(".box").innertext;
 box.style.color = "red";
 
+let div = document.createElement("div");
+div.innerHTML = "I have been inserted div";
+div.setAttribute("class", "created");
+document.querySelector(".container").append(div);
+
 document.querySelector(".container").outerHTML;
 document.querySelector(".container").tagName;
 document.querySelector(".container").nodeName;
@@ -11,4 +16,7 @@ document.querySelector(".box").hasAttribute("style");
 document.querySelector(".box").getAttribute("style");
 document.querySelector(".box").setAttribute("style", "display:inline");
 document.querySelector(".box").attributes;
-document.querySelector(".box").removeAttribute("sytle")
+document.querySelector(".box").removeAttribute("sytle");
+document.designMode = "on";
+document.querySelector(".container").dataset;
+document.querySelector(".box").dataset;
