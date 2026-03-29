e52487a9dd938f401aaab36481b7b9d8c3ac3444
Mohaideen27 <mohaideenmech27@gmail.com>
2026-03-29 14:00:35 +0530

    PRACTICING JAVASCRIPT IMP FUNCTIONS

---
+++ JAVASCRIPT/IMP. FUNCTIONS/script.js
@@ -0,0 +1,14 @@
+let box = document.querySelector(".box").innertext;
+box.style.color = "red";
+
+document.querySelector(".container").outerHTML;
+document.querySelector(".container").tagName;
+document.querySelector(".container").nodeName;
+document.querySelector(".container").textContent;
+document.querySelector(".box").hidden = true;
+document.querySelector(".box").innerHTML = "hey i am sameer";
+document.querySelector(".box").hasAttribute("style");
+document.querySelector(".box").getAttribute("style");
+document.querySelector(".box").setAttribute("style", "display:inline");
+document.querySelector(".box").attributes;
+document.querySelector(".box").removeAttribute("sytle")
