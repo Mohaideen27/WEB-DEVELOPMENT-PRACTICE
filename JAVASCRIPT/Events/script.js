4b2b19e3b87b514f2b800da03d09a2604199ae36
Mohaideen27 <mohaideenmech27@gmail.com>
2026-03-30 23:56:45 +0530

    PRACTICING JAVASCRIPT EVENTS

---
+++ JAVASCRIPT/Events/script.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById("btn");
+
+// btn.addEventListener("click", () => {
+//   alert("I was clicked");
+// });
+
+// btn.addEventListener("dblclick", () => {
+//   alert("I was double clicked");
+// });
+
+// btn.addEventListener("click", () => {
+//   document.querySelector(".box").innerHTML = "<b>Clicked</b>";
+// });
+// btn.addEventListener("contextmenu", () => {
+//   alert("right clicked");
+// });
+
+btn.addEventListener("keydown", (e) => {
+  console.log(e);
+});
