1c24b730ded952b6e5d0d1f32a0e0cfd8bd9f6ad
Mohaideen27 <mohaideenmech27@gmail.com>
2026-03-30 23:00:10 +0530

    PRACTICED JAVASCRIPT IMP FUNCTION

--- JAVASCRIPT/IMP. FUNCTIONS/script.js
+++ JAVASCRIPT/IMP. FUNCTIONS/script.js
@@ -1,22 +1,41 @@
-let box = document.querySelector(".box").innertext;
-box.style.color = "red";
+// let box = document.querySelector(".box").innertext;
+// box.style.color = "red";
+// let div = document.createElement("div");
+// div.innerHTML = "I have been inserted div <b> by sameer</b>";
+// div.setAttribute("class", "created");
+// document.querySelector(".container").append(div);
+// document.querySelector(".container").before(div);
+// document.querySelector(".container").outerHTML;
+// document.querySelector(".container").tagName;
+// document.querySelector(".container").nodeName;
+// document.querySelector(".container").textContent;
+// document.querySelector(".box").hidden = true;
+// document.querySelector(".box").innerHTML = "hey i am sameer";
+// document.querySelector(".box").hasAttribute("style");
+// document.querySelector(".box").getAttribute("style");
+// document.querySelector(".box").setAttribute("style", "display:inline");
+// document.querySelector(".box").attributes;
+// document.querySelector(".box").removeAttribute("sytle");
+// document.designMode = "on";
+// document.querySelector(".container").dataset;
+// document.querySelector(".box").dataset;
 
-let div = document.createElement("div");
-div.innerHTML = "I have been inserted div";
-div.setAttribute("class", "created");
-document.querySelector(".container").append(div);
+// let cons = document.querySelector(".container");
+// cons.insertAdjacentHTML(
+//   "afterend",
+//   "<b>I am under the water, please help me.... iuooooooooo</b>",
+// );
+// let cons = document.querySelector(".container");
+// cons.insertAdjacentHTML(
+//   "beforebegin",
+//   "<b>I am under the water, please help me.... iuooooooooo</b>",
+// );
 
-document.querySelector(".container").outerHTML;
-document.querySelector(".container").tagName;
-document.querySelector(".container").nodeName;
-document.querySelector(".container").textContent;
-document.querySelector(".box").hidden = true;
-document.querySelector(".box").innerHTML = "hey i am sameer";
-document.querySelector(".box").hasAttribute("style");
-document.querySelector(".box").getAttribute("style");
-document.querySelector(".box").setAttribute("style", "display:inline");
-document.querySelector(".box").attributes;
-document.querySelector(".box").removeAttribute("sytle");
-document.designMode = "on";
-document.querySelector(".container").dataset;
-document.querySelector(".box").dataset;
+// let box = document.querySelector(".box");
+// box.remove();
+// document.querySelector(".container").classList
+// document.querySelector(".container").className
+// document.querySelector(".container").classList.add("box1");
+// document.querySelector(".container").classList.remove("box1")
+// document.querySelector(".container").classList.toggle("box1");
+// document.querySelector(".container").classList.toggle("box1");
