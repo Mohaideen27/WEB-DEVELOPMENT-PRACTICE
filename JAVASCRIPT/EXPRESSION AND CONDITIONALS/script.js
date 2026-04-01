954e9f963be04048c32d38707364e099ef19f78b
Mohaideen27 <mohaideenmech27@gmail.com>
2026-04-01 23:40:46 +0530

    PRACTICED JAVASCRIPT OPERATORS

---
+++ JAVASCRIPT/EXPRESSION AND CONDITIONALS/script.js
@@ -0,0 +1,48 @@
+// ARTHIMETIC OPERATOR
+console.log(4 + 5);
+console.log(5 - 6);
+console.log(4 * 2);
+console.log(2.2 * 2);
+console.log(5.0 * 2);
+console.log(5.0 * 2.0);
+console.log(5 ** 2);
+console.log(4 / 2);
+console.log(5 / 2);
+console.log(3 % 2);
+let a = 10;
+console.log(a++);
+console.log(a--);
+
+// ASSIGNMENT OPERATOR
+let b = 20;
+let c = 2;
+console.log((b += c));
+console.log((b -= c));
+console.log((b *= c));
+console.log((b /= c));
+console.log((b %= c));
+console.log((b **= c));
+
+// COMPARISON OPERATOR
+d = 32.2;
+e = 20;
+f = "20";
+console.log(d == e);
+console.log(d != e);
+console.log(e === f);
+console.log(e !== f);
+console.log(e > f);
+console.log(e > f);
+console.log(e >= d);
+console.log(e <= d);
+// TERNARY OPERATOR
+// SYNTAX: CONDITION?TRUE:FALSE
+console.log(e == d ? "right" : "wrong");
+
+// LOGICAL OPERATOR
+
+console.log(e < d && e > d ? "possible" : "not possible");
+console.log(e < d || e > d ? "possible" : "not possible");
+// console.log(not operator)
+
+
