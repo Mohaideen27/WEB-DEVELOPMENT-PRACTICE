86a308e54a204dd12d179d1a520bfe369edc6cc4
Mohaideen27 <mohaideenmech27@gmail.com>
2026-04-03 00:00:47 +0530

    PRACTICED STRING

---
+++ JAVASCRIPT/STRINGS/script.js
@@ -0,0 +1,58 @@
+let name = "sameer";
+
+console.log(name.length);
+console.log(name, "is mass");
+
+let name1 = "SAMEER";
+
+console.log(name1);
+
+let name2 = `sameer`;
+
+console.log(name2);
+
+let sentence = `the name is "is" sameer`;
+
+console.log(sentence);
+
+let a = `this is ${name1}`;
+console.log(a);
+
+// let escape='adam d' angelo'
+// console.log(escape);
+
+let es = `adam d' angelo`;
+console.log(a);
+
+let es2 = 'Adam D"Angelo';
+console.log(es, es2);
+
+// STRING PROPERTIES AND METHODS
+
+let sa = "sameer";
+console.log(sa.length);
+
+let di = "dinesh";
+console.log(di.toUpperCase());
+
+let ha = "HARISH";
+console.log(ha.toLowerCase());
+
+let j = "java";
+console.log(j.slice(2, 4));
+let js = "JAVASCRIPT";
+console.log(js.slice(4));
+
+let n = "My name is sameer";
+n1 = n.replace("sameer", "samee");
+console.log(n1);
+
+let s = "string";
+console.log(s[2]);
+
+// let t = "My   Name   Is Samee";
+// console.log(t.trim());
+
+let co1 = "MASTER";
+let co2 = "SAMEER";
+console.log(co1.concat(" ",co2));
