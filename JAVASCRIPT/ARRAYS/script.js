2233b6727310e4ccdd54e6239e7dcf9986b1d88a
Mohaideen27 <mohaideenmech27@gmail.com>
2026-04-03 12:33:24 +0530

    PRACTICING ARRAY AND ARRAY METHODS

---
+++ JAVASCRIPT/ARRAYS/script.js
@@ -0,0 +1,61 @@
+// ARRAY
+const f = ["banana", "apple", "grapes"];
+const a1 = [7, "Harry", false];
+console.log(f);
+console.log(a1);
+
+// ACCESSING VALUES'
+let n = [1, 2, 3, 4, 4];
+console.log(n);
+console.log(n[4]);
+// FINDING THE LENGTH
+console.log(n.length);
+console.log(f[0].length);
+// CHANGING THE VALUES
+a1[0] = 6;
+console.log(a1);
+// typeof
+console.log(typeof n);
+// ARRAY METHODS
+// TO STRING()
+let b = [2, 3, 4, 7];
+console.log(b.toString());
+
+// JOIN()
+console.log(b.join("+"));
+// POP()
+b1 = b.pop();
+console.log(b);
+console.log(b1);
+
+// PUSH()
+
+let a2 = [5, 46, 6, 56, 4];
+a3 = a2.push(9);
+console.log(a2);
+console.log(a3);
+// SHIFT()
+a4 = a2.shift();
+console.log(a2);
+console.log(a4);
+// UNSHIFT()
+a5 = a2.unshift(1);
+console.log(a2);
+console.log(a5);
+// DELETE
+delete a2[2];
+console.log(a2);
+// CONCAT()
+// SORT()
+// SPLICE()
+// SLICE()
+// REVERSE()
+
+// LOOPING THROUGH ARRAYS
+// FOREACHLOOP
+// Map()
+// FILTER()
+// REDUCE METHOD
+// ARRAY FROM
+// FOR OF
+// FOR IN
