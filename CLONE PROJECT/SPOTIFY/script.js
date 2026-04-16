8d6d057e6f953972bd988cf7c51156ea11984eea
Mohaideen27 <mohaideenmech27@gmail.com>
2026-04-17 02:01:31 +0530

    SEEKBAR FUNCTIONS UPDATED

--- CLONE PROJECT/SPOTIFY/script.js
+++ CLONE PROJECT/SPOTIFY/script.js
@@ -1,5 +1,17 @@
 console.log("Lets start working on Javascript");
 let currentSong = new Audio();
+function formatTime(seconds) {
+  // Ensure we are working with a positive number
+  let totalSeconds = Math.floor(seconds);
+
+  let minutes = Math.floor(totalSeconds / 60);
+  let remainingSeconds = totalSeconds % 60;
+
+  // Use .padStart to ensure the seconds always show two digits
+  let formattedSeconds = remainingSeconds.toString().padStart(2, "0");
+
+  return `${minutes}:${formattedSeconds}`;
+}
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/");
   let response = await a.text();
@@ -16,16 +28,20 @@ async function getSongs() {
   return songs;
 }
 
-const playMusic = (track) => {
+const playMusic = (track, pause = false) => {
   // let audio = new Audio("/songs/" + track);
   currentSong.src = "/songs/" + track;
-  currentSong.play();
+  if (!pause) {
+    currentSong.play();
+  }
   play.src = "pause.svg";
+  document.querySelector(".songinfo").innerHTML = decodeURI(track);
+  document.querySelector(".songtime").innerHTML = "00:00/00:00";
 };
 
 async function main() {
   let songs = await getSongs();
-  console.log(songs);
+  playMusic(songs[0], true);
 
   let songUL = document
     .querySelector(".songlist")
@@ -82,6 +98,21 @@ async function main() {
       currentSong.pause();
       play.src = "media-player-ui-button-play.svg";
     }
+  });
+
+  // LISTEN FOR TIMEUPDATE EVENT
+  currentSong.addEventListener("timeupdate", () => {
+    document.querySelector(".songtime").innerHTML =
+      `${formatTime(currentSong.currentTime)}/${formatTime(currentSong.duration)}`;
+    document.querySelector(".circle").style.left =
+      (currentSong.currentTime / currentSong.duration) * 100 + "%";
+  });
+
+  // ADD AN EVENT LISTENER TO MOVE THE SEEKBAR CIRCLE
+  document.querySelector(".seekbar").addEventListener("click", (e) => {
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+    currentSong.currentTime = (currentSong.duration * percent) / 100;
   });
 }
 main();
