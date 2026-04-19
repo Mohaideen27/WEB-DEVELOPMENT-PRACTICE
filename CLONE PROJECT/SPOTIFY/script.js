2046f32c6d394bbee7e8853d990a175686c87e47
Mohaideen27 <mohaideenmech27@gmail.com>
2026-04-19 23:53:15 +0530

    HAMBURGER FUNCTION UPDATED

--- CLONE PROJECT/SPOTIFY/script.js
+++ CLONE PROJECT/SPOTIFY/script.js
@@ -120,4 +120,7 @@ async function main() {
 document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".left").style.left = "0%";
 });
+document.querySelector(".cross").addEventListener("click", () => {
+  document.querySelector(".left").style.left = "-100%";
+});
 main();
