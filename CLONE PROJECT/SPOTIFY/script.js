52d465d916f8c3a261a95fd944e5f18909fb3fa1
Mohaideen27 <mohaideenmech27@gmail.com>
2026-04-20 20:48:06 +0530

    UPDATED RESPONSIVE DESIGN

--- CLONE PROJECT/SPOTIFY/script.js
+++ CLONE PROJECT/SPOTIFY/script.js
@@ -121,6 +121,6 @@ document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector(".left").style.left = "0%";
 });
 document.querySelector(".cross").addEventListener("click", () => {
-  document.querySelector(".left").style.left = "-100%";
+  document.querySelector(".left").style.left = "-120%";
 });
 main();
