c4918a8bb4312c6ee190ff56e7fe85eb7c398454
Mohaideen27 <mohaideenmech27@gmail.com>
2026-05-05 22:54:35 +0530

    LEARNING JAVASCRIPT FS MODULE

--- JAVASCRIPT/FS MODULE/main.js
+++ JAVASCRIPT/FS MODULE/main.js
@@ -4,7 +4,10 @@ const fs = require("fs");
 
 console.log("starting");
 // fs.writeFileSync("confidential.txt", "Naa dan da leo das");
-fs.writeFile ("sameer.txt", "Sameer is a naughty boy", () => {
+fs.writeFile("sameer.txt", "Sameer is a naughty boy", () => {
   console.log("done");
+  fs.readFile("sameer.txt", (error, data) => {
+    console.log(error, data.toString());
+  });
 });
 console.log("ending");
