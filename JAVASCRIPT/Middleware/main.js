06ea9f4e0f9d43bfd625c2d044c553d3835eddfb
Mohaideen27 <mohaideenmech27@gmail.com>
2026-05-15 23:04:03 +0530

    LEARNED JAVASCIRPT MIDDLEWARE

--- JAVASCRIPT/Middleware/main.js
+++ JAVASCRIPT/Middleware/main.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const blog = require("./routes/blog.js");
 const fs = require("fs");
 
+app.use("./blog", blog);
 app.use(express.static("public"));
 // MIDDLEWARE1
 app.use((req, res, next) => {
+  console.log(req.headers);
+  req.sameer = "i am sameer";
   fs.appendFileSync("logs.txt", `${Date.now()} is a ${req.method} \n`);
   console.log(`${Date.now()} is a ${req.method}`);
   //   res.send("Hacked by Middleware 1");
@@ -22,7 +26,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.get("/about", (req, res) => {
-  res.send("Hello about!");
+  res.send("Hello about!" + req.sameer);
 });
 app.get("/contact", (req, res) => {
   res.send("Hello contact!");
