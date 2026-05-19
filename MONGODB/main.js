7154a2d8eeeb0530275284da920cbc7af05ca78f
Mohaideen27 <mohaideenmech27@gmail.com>
2026-05-19 10:39:33 +0530

    LEARNED MONGOOSE IN JS

--- MONGODB/main.js
+++ MONGODB/main.js
@@ -1,13 +1,25 @@
 import mongoose from "mongoose";
 import express from "express";
-let a = await mongoose.connect("mongodb://localhost:27017/todo");
+import { todo } from "./models/todo.js";
+
+let conn = await mongoose.connect("mongodb://localhost:27017/todo");
 // const express = require("express");
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  const newtodo = new todo({
+    desc: "Description of this todo",
+    isDone: false,
+    days: 24,
+  });
+  newtodo.save();
   res.send("Hello World!");
+});
+app.get("/a", async (req, res) => {
+  let newtodo = await todo.findOne({});
+  res.json({ title: newtodo.title, desc: newtodo.desc });
 });
 
 app.listen(port, () => {
