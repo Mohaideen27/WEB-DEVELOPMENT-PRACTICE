fd2a1972a0945b2292c834de669ca5dd400b3a9e
Mohaideen27 <mohaideenmech27@gmail.com>
2026-05-29 03:24:01 +0530

    PRACTICING NEXTJS

---
+++ NEXTJS/my-app/app/contact/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const contact = () => {
+  return <div>hi soliders</div>;
+};
+
+export default contact;
---
+++ NEXTJS/my-app/app/page.js
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+import Navbar from "@/public/component/Navbar";
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div>I am homepage</div>
+    </div>
+  );
+}
