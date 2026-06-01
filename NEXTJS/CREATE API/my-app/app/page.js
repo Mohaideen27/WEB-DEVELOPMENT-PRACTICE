99543cede22865a28e51176f17695d9928aa9277
Mohaideen27 <mohaideenmech27@gmail.com>
2026-06-01 16:45:20 +0530

    PRACTICING CREATING API IN NEXTJS

---
+++ NEXTJS/CREATE API/my-app/app/page.js
@@ -0,0 +1,20 @@
+"use client";
+
+export default function Home() {
+  const handleClick = async () => {
+    let data = { name: "sameer", role: "coder" };
+    let a = await fetch("/api/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    let res = await a.json();
+    console.log(res);
+  };
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Next.js Api routes demo</h1>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+}
